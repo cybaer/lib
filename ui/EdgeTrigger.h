dd59a769c815bd6a3e7df2402c10c1ba371b048d
cybaer <s.k.g@gmx.de>
2018-03-06 08:05:33 +0100

    gger-h added Bitte geben Sie eine Commit-Beschreibung für Ihre Änderungen ein. Zeilen,

---
+++ ui/EdgeTrigger.h
@@ -0,0 +1,55 @@
+/*
+ * EdgeTrigger.h
+ *
+ *  Created on: 06.03.2018
+ *      Author: cybaer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef EDGETRIGGER_H_
+#define EDGETRIGGER_H_
+
+#include "avrlib/gpio.h"
+using namespace avrlib;
+
+template<typename Input, uint8_t ActiveLevel=0>
+class EdgeTrigger
+{
+public:
+  static inline void init(void)
+  {
+    Input::set_mode(DIGITAL_INPUT);
+    if(ActiveLevel == 0) Input::High();
+  }
+  static inline bool isTriggered(void)
+  {
+    uint8_t in = Input::value();
+    bool ret = in != OldValue && in == ActiveLevel;
+    OldValue = in;
+    return ret;
+  }
+private:
+  static uint8_t OldValue;
+};
+
+template<typename Input, uint8_t ActiveLevel>
+uint8_t EdgeTrigger<Input, ActiveLevel>::OldValue;
+
+
+
+
+
+
+
+#endif /* EDGETRIGGER_H_ */
